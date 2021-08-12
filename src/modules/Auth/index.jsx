7c8d1200432c5fcce1974ce1a30c0a4a0c34de1c
Stanley Obumneme Dukor <stanleydukor@gmail.com>
2021-08-11 20:50:46 -0400

    Update index.jsx

--- src/modules/Auth/index.jsx
+++ src/modules/Auth/index.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
-import Loading from "../../components/Loading";
+import Loading from "components/Loading";
 import { authRoutes } from "routes/routes-list";
 const Signin = React.lazy(() => import("./pages/Signin"));
 
