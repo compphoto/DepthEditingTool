1ec22c7cce5ec48f9c8db911daedd146bfbc54a2
Stanley Dukor <stanleydukor@gmail.com>
2021-11-21 13:36:28 -0800

    add bar chart

--- src/components/HistViewer/index.jsx
+++ src/components/HistViewer/index.jsx
@@ -2,15 +2,11 @@ import React, { Component, createRef } from "react";
 import { connect } from "react-redux";
 import { imageActions } from "store/image";
 import { selectors as imageSelectors } from "store/image";
-import { Grid, Typography } from "@material-ui/core";
 import RangeSlider from "./rangeslider";
 import HistViewerStyle from "./style";
 import { getImageData } from "utils/drawHistogram";
 
-const prices = [];
-for (let i = 0; i < 500; i++) {
-  prices.push(Math.floor(Math.random() * 80) + 1);
-}
+const prices = [2, 4, 5, 6, 7, 8];
 
 class HistViewer extends Component {
   constructor() {
@@ -32,7 +28,6 @@ class HistViewer extends Component {
   }
   render() {
     const { data } = this.state;
-    console.warn(prices);
     return (
       <HistViewerStyle>
         <RangeSlider data={data || prices} />
